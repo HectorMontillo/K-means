0e2dc843bd9d8b6b9ac1ef907c6a5813a43e12dd
Hector Montillo <hector.montillo@utp.edu.co>
2020-04-08 22:45:56 -0500

    Comunicacion entre el fan y el sink establecida

--- fan.py
+++ fan.py
@@ -2,18 +2,25 @@ import zmq
 import sys
 import pandas as pd
 import numpy as np
+import math
+
 class K_means_Fan:
-	def __init__(self,data_set,k,sink_address,port_worker='5555',port_sink='5556',max_iterations=1000, n=10,tolerance=0.001, init_samples = None):
+	def __init__(self,data_set,k,target_columns,sink_address,port_worker='5555',port_sink='5556',max_iterations=1000, n=10,tolerance=0.001):
 		#Initial data
-		self.data_set = self.read_data(data_set)
+		self.data_set = self.read_data(data_set)[target_columns]
 		self.k = k #number of clusters
 		self.n = n #number of samples to send to workers
+		self.target_columns = target_columns
 		self.max_iterations = max_iterations
 		self.tolerance = tolerance
-		self.init_samples = init_samples # number of samples to be read to calculate initial centroids
+		#self.init_samples = init_samples # number of samples to be read to calculate initial centroids
+
+		#data
+		self.number_samples = self.data_set.shape[0]
+		self.number_tasks = math.ceil(self.number_samples/self.n)
 
 		#Clusters
-		self.clusters = list()
+		self.clusters = None
 
 
 		#Zqm push pull implementation
@@ -31,32 +38,57 @@ class K_means_Fan:
 				#Initialize
 	def connect(self):
 		self.workers.bind(f"tcp://*:{self.port_worker}")
-		self.sink_push.bind(f"tcp://*:{self.port_sink}")
-		self.sink_pull.connect(f"tcp://{self.sink_address}")
+		self.sink_push.connect(f"tcp://{self.sink_address}")
+		self.sink_pull.bind(f"tcp://*:{self.port_sink}")
 
 	def read_data(self,data_set):
 		return pd.read_csv(data_set)
 
 
 	def run(self):
-		print(type(self.data_set))
-		print(self.data_set.shape)
-		for i in range(self.max_iterations):
+		print("Press enter when workers and sink are ready...")
+		_ = input()
+		print("sending tasks to workers")
+
+
+		print(f"Number of samples: {self.number_samples}, Number of task per iteration: {self.number_tasks}")
+		
+		self.initialize_clusters()
+		iterations = 0
+		while True:
+			
+			# send sink the number of results
+			self.sink_push.send(str(self.number_tasks).encode('ascii'))
+
+			# send workers the taks
+
+			# recive the new clusters from sink
+			message = self.sink_pull.recv()
+			print(message)
+
+			# compare clusters to stop
+			if(iterations >= self.max_iterations):
+				break
+			iterations+=1
 			break
+
 		
 	def initialize_clusters(self):
-		pass
+		self.clusters = self.data_set.sample(n=self.k).to_dict(orient='split')['data']
+		print(self.clusters)
+		#print(self.clusters.to_dict(orient='split'))
 
 
 if __name__ == "__main__":
 		try:
 				data_set = sys.argv[1]
-				k = sys.argv[2]
-				sink_address = sys.argv[3]
+				target_columns = sys.argv[2].strip('[]').split(',')
+				k = int(sys.argv[3])
+				sink_address = sys.argv[4]
 		except IndexError:
-				print("'data_set','k' or 'port' are mising!")
+				print("Mising arguments!")
 
-		fan = K_means_Fan(data_set,int(k),sink_address)
+		fan = K_means_Fan(data_set,k,target_columns,sink_address)
 		fan.run()
 		
 
--- sink.py
+++ sink.py
@@ -2,6 +2,43 @@ import sys
 import time
 import zmq
 
+
+class K_means_Sink:
+	def __init__(self,port_pull,fan_address):
+		self.port_pull = port_pull
+		self.fan_address = fan_address
+
+		self.context = zmq.Context()
+
+		self.fan_pull = self.context.socket(zmq.PULL)
+		self.fan_push = self.context.socket(zmq.PUSH)
+
+		self.connect()
+	
+	def connect(self):
+		self.fan_pull.bind(f"tcp://*:{self.port_pull}")
+		self.fan_push.connect(f"tcp://{self.fan_address}")
+
+	def run(self):
+		print("Sink waiting")
+		message = self.fan_pull.recv()
+		print(message)
+		self.fan_push.send(b"Llego")
+
+
+if __name__ == "__main__":
+	try:
+		port_pull = sys.argv[1]
+		fan_address = sys.argv[2]
+	except IndexError:
+		print("Arguments missing!")
+	
+	sink = K_means_Sink(port_pull,fan_address)
+	sink.run()
+		
+
+
+'''
 context = zmq.Context()
 
 fan = context.socket(zmq.PULL)
@@ -15,15 +52,16 @@ tstart = time.time()
 
 # Process 100 confirmations
 for task in range(100):
-    print(task)
-    s = fan.recv()
-    if task % 10 == 0:
-        sys.stdout.write(':')
-    else:
-        sys.stdout.write('.')
-    sys.stdout.flush()
-    print("-")
+		print(task)
+		s = fan.recv()
+		if task % 10 == 0:
+				sys.stdout.write(':')
+		else:
+				sys.stdout.write('.')
+		sys.stdout.flush()
+		print("-")
 
 # Calculate and report duration of batch
 tend = time.time()
 print("Total elapsed time: %d msec" % ((tend-tstart)*1000))
+'''
